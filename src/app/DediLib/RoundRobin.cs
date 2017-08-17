16ea7108a6247d637574292d806576c47792e51e
Hans Wolff <hanswolff@users.noreply.github.com>
2017-08-17 19:57:19 +0200

    Upgrade to .NET Standard 2.0

--- src/app/DediLib/RoundRobin.cs
+++ src/app/DediLib/RoundRobin.cs
@@ -37,14 +37,16 @@ namespace DediLib
         }
 
         private readonly object _cycleLock = new object();
-        public void Cycle()
+        public T Cycle()
         {
             lock (_cycleLock)
             {
                 var currentRound = _currentRound;
                 var nextRound = (currentRound + 1) % _rounds;
-                _onSelected(_items[nextRound]);
+                var newCurrent = _items[nextRound];
+                _onSelected(newCurrent);
                 _currentRound = nextRound;
+                return newCurrent;
             }
         }
 
